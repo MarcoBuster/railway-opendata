cb0a173a2187515f1c6b867efc56f403b412a114
Marco Aceti <mail@marcoaceti.it>
2023-03-14 00:23:18 +0100

    Log to stdout, not stderr

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import logging
 import os
+import sys
 
 import src.scraper.main as scraper
 
 logging.basicConfig(
+    stream=sys.stdout,
     format="[%(asctime)s - %(levelname)s] %(message)s",
     level=logging.INFO if not os.environ.get("DEBUG", False) else logging.DEBUG,
 )
