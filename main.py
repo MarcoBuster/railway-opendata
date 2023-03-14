e223f22e7cd2b10e4daab4ea61a3ad9b94894113
Marco Aceti <mail@marcoaceti.it>
2023-03-14 20:54:52 +0100

    Disable hash seed randomization

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import logging
 import os
 import sys
 
+hashseed = os.getenv("PYTHONHASHSEED")
+if not hashseed or hashseed != "0":
+    logging.critical(
+        "Hash seed randomization is not disabled. "
+        "Please disable it by setting PYTHONHASHSEED=0 environment variable"
+    )
+    sys.exit(1)
+
 import src.scraper.main as scraper
 
 logging.basicConfig(
