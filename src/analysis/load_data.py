507a655e072f422da27897d2a37a342bfed238a9
Marco Aceti <mail@marcoaceti.it>
2023-03-24 16:35:12 +0100

    Add data loader

---
+++ src/analysis/load_data.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import pandas as pd
+
+from src.const import RailwayCompany
+
+
+def read_train_csv(file: Path) -> pd.DataFrame:
+    """Load train CSV to a pandas dataframe
+
+    Args:
+        file (Path): the train CSV file path
+
+    Returns:
+        pd.DataFrame: the loaded dataframe
+    """
+
+    df: pd.DataFrame = pd.read_csv(
+        file,
+        parse_dates=[
+            "day",
+            "arrival_expected",
+            "arrival_actual",
+            "departure_expected",
+            "departure_actual",
+        ],
+        infer_datetime_format=True,
+    )
+    df.client_code = df.client_code.apply(RailwayCompany.from_code)  # type: ignore
+    return df.loc[(df.phantom == False) & (df.trenord_phantom == False)].drop(
+        ["phantom", "trenord_phantom"], axis=1
+    )
+
+
+def read_station_csv(file: Path) -> pd.DataFrame:
+    """Load station CSV to a pandas dataframe
+
+    Args:
+        file (Path): the station CSV file path
+
+    Returns:
+        pd.DataFrame: the loaded dataframe
+    """
+
+    return pd.read_csv(file)
--- src/const.py
+++ src/const.py
@@ -1,4 +1,25 @@
+from enum import Enum
+
 from dateutil import tz
 
 # Global timezone used in all datetime calls.
 TIMEZONE = tz.gettz("GMT+1")
+
+
+class RailwayCompany(Enum):
+    """Italian railway companies codes."""
+
+    TRENITALIA_AV = 1
+    TRENITALIA_REG = 2
+    TRENITALIA_IC = 4
+    TPER = 18
+    TRENORD = 63
+    OBB = 64
+    OTHER = -1
+
+    @classmethod
+    def from_code(cls, code: int) -> "RailwayCompany":
+        try:
+            return cls(code)
+        except ValueError:
+            return cls.OTHER
