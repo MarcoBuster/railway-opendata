8fe09e573691e874ebbf9b7d5fcfdf344b4ce7df
Marco Aceti <mail@marcoaceti.it>
2023-04-09 12:22:22 +0200

    Enhance datetime parsing

--- src/analysis/load_data.py
+++ src/analysis/load_data.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 from src.const import RailwayCompany
@@ -15,22 +17,41 @@ def read_train_csv(file: Path) -> pd.DataFrame:
         pd.DataFrame: the loaded dataframe
     """
 
-    df: pd.DataFrame = pd.read_csv(
-        file,
-        parse_dates=[
-            "day",
-            "arrival_expected",
-            "arrival_actual",
-            "departure_expected",
-            "departure_actual",
-        ],
-        infer_datetime_format=True,
-    )
+    df: pd.DataFrame = pd.read_csv(file)
+
+    @np.vectorize
+    def _parse_dt(_string: str | None) -> datetime | None:
+        try:
+            if not isinstance(_string, str):
+                return None
+            return datetime.fromisoformat(_string)
+        except ValueError:
+            return None
+
+    # Parse datetimes
+    for dt_field in [
+        "arrival_expected",
+        "arrival_actual",
+        "departure_expected",
+        "departure_actual",
+    ]:
+        df[dt_field] = (
+            df[dt_field]
+            .apply(_parse_dt)
+            .astype("object")
+            .where(df[dt_field].notnull(), None)
+        )
+
+    df.day = pd.to_datetime(df.day)
+
+    # Map client codes
     df.client_code = df.client_code.apply(RailwayCompany.from_code)  # type: ignore
-    df.day = pd.to_datetime(df.day.apply(lambda dt: dt.date()))
-    return df.loc[(df.phantom == False) & (df.trenord_phantom == False)].drop(
+
+    # Exclude phantom data
+    df = df.loc[(df.phantom == False) & (df.trenord_phantom == False)].drop(
         ["phantom", "trenord_phantom"], axis=1
     )
+    return df
 
 
 def read_station_csv(file: Path) -> pd.DataFrame:
